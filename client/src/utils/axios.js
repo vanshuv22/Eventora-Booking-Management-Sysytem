52cb2ff988f5a7ec497e5ca652c1b3df1b7e1831
vanshuv22 <vermavanshikaoffical@gnail.com>
2026-06-28 15:37:38 +0530

    removed dependenciend and node modules

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://eventora-booking-management-system.onrender.com//api',
+  baseURL: 'https://eventora-booking-management-system.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
